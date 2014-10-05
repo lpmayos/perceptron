f7aba2573403445d5800ebf10434c113451b0674
Laura Pérez Mayos <lpmayos@gmail.com>
2014-10-05 22:27:30 +0200

    perceptron learning algorithm implemented

--- perceptron.py
+++ perceptron.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 class Perceptron(object):
-    """
+    """ Perceptron Learning Algorithm implementation
     """
 
     def __init__(self, N):
@@ -11,24 +11,24 @@ class Perceptron(object):
         using this training set
         """
         # define two random points
-        xA, yA, xB, yB = self.generate_random_numbers(4)
+        xA, yA, xB, yB = self._generate_random_numbers(4)
         # define a line given the two points
         self.V = numpy.array([xB * yA - xA * yB, yB - yA, xA - xB])
         # generate a training set using the line
-        self.X = self.generate_training_set(N)
+        self.X = self._generate_training_set(N)
 
-    def generate_random_numbers(self, N):
+    def _generate_random_numbers(self, N):
         """ return N random numbers within [-1, 1]
         """
         return [numpy.random.uniform(-1, 1) for i in range(N)]
 
-    def generate_training_set(self, N):
+    def _generate_training_set(self, N):
         """ generates N random points within [-1, 1] x [-1, 1] and evaluates
         them with self.V
         """
         X = []
         for i in range(N):
-            xA, yA = self.generate_random_numbers(2)
+            xA, yA = self._generate_random_numbers(2)
             x = numpy.array([1, xA, yA])
             s = int(numpy.sign(self.V.T.dot(x)))
             X.append((x, s))
@@ -47,20 +47,58 @@ class Perceptron(object):
         cols = {1: 'r', -1: 'b'}
         for x, s in self.X:
             plt.plot(x[1], x[2], cols[s] + 'o')
-        # if mispts:
-        #     for x, s in mispts:
-        #         plt.plot(x[1], x[2], cols[s] + '.')
-        # if vec is not None:
-        #     aa, bb = -vec[1] / vec[2], -vec[0] / vec[2]
-        #     plt.plot(l, aa * l + bb, 'g-', lw=2)
-        # if save:
-        #     if not mispts:
-        #         plt.title('N = %s' % (str(len(self.X))))
-        #     else:
-        #         plt.title('N = %s with %s test points' % (str(len(self.X)), str(len(mispts))))
-        #     plt.savefig('p_N%s' % (str(len(self.X))), dpi=200, bbox_inches='tight')
-        plt.show()
+        if mispts:
+            for x, s in mispts:
+                plt.plot(x[1], x[2], cols[s] + '.')
+        if vec is not None:
+            aa, bb = -vec[1] / vec[2], -vec[0] / vec[2]
+            plt.plot(l, aa * l + bb, 'g-', lw=2)
+        if save:
+            if not mispts:
+                plt.title('N = %s' % (str(len(self.X))))
+            else:
+                plt.title('N = %s with %s test points' % (str(len(self.X)), str(len(mispts))))
+            plt.savefig('p_N%s' % (str(len(self.X))), dpi=200, bbox_inches='tight')
+        # plt.show()
+
+    def _has_misclassified_points(self, vec):
+        """ returns the fraction of misclassified points
+        """
+        X, N = self.X, len(self.X)
+        for x, s in X:
+            if int(numpy.sign(vec.T.dot(x))) != s:
+                return True
+        return False
+ 
+    def _choose_miscl_point(self, vec):
+        """ returns the first misclassified point it finds
+        """
+        X = self.X
+        for x, s in X:
+            if int(numpy.sign(vec.T.dot(x))) != s:
+                return (x, s)
+ 
+    def pla(self, save=False):
+        """ Perceptron Learnin Algorithm
+        """
+        # initialize the weigths to zeros
+        X, N = self.X, len(self.X)
+        w = numpy.zeros(3)
+        iteration = 0
+        # iterate until all points are correctly classified
+        while self._has_misclassified_points(w):
+            iteration += 1
+            # pick random misclassified point
+            x, s = self._choose_miscl_point(w)
+            # update weights
+            w += s * x
+            if save:
+                self.plot(vec=w)
+                plt.title('N = %s, Iteration %s\n' % (str(N), str(iteration)))
+                plt.savefig('p_N%s_it%s' % (str(N), str(iteration)), dpi=200, bbox_inches='tight')
+        self.w = w
 
 if __name__ == "__main__":
     p = Perceptron(20)
     p.plot()
+    p.pla(save=True)
